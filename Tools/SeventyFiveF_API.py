e75c6a30e652985c77d43eb37be8ca56185fc0cd
Peter Knall <pknall@aesbuildingservices.com>
2025-11-16 09:53:13 -0500

    Moved get_all_site_ids to API Tools.

--- Tools/SeventyFiveF_API.py
+++ Tools/SeventyFiveF_API.py
@@ -3,8 +3,16 @@ import SeventyFiveF.ReadByFilter as ReadByFilter
 import pandas as pd
 
 class SeventyFiveF_API:
-
+    """
+    Commonly used functions for the 75F API.
+    """
     def __init__(self, username, password, subscriptionKey):
+        """
+        Frequently used functions for the 75F API.
+        :param username: Facilisight Username
+        :param password: Facilisight Password
+        :param subscriptionKey: API Key from 75F API Management portal
+        """
         self.username = username
         self.password = password
         self.subscriptionKey = subscriptionKey
@@ -19,3 +27,10 @@ class SeventyFiveF_API:
         result = reader.post()
         df = pd.DataFrame(result["rows"])  # Import into a data frame
         return df
+
+    def get_all_site_ids_df(self):
+        """
+        Retrieves all the sites the user has access to.
+        :return: Pandas DataFrame
+        """
+        return self.get_df_by_filter("building and equip")
