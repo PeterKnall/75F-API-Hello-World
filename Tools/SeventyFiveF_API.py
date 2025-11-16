1d01564f90dc695519e44422f16912d4e89e05ce
Peter Knall <pknall@aesbuildingservices.com>
2025-11-16 09:47:18 -0500

    Moved and renamed "get_df" to API Tool class.

--- Tools/SeventyFiveF_API.py
+++ Tools/SeventyFiveF_API.py
@@ -1,7 +1,21 @@
-
+import os
+import SeventyFiveF.ReadByFilter as ReadByFilter
+import pandas as pd
 
 class SeventyFiveF_API:
 
-    def __init__(self):
-        pass
+    def __init__(self, username, password, subscriptionKey):
+        self.username = username
+        self.password = password
+        self.subscriptionKey = subscriptionKey
 
+    def get_df_by_filter(self, query_string):
+        """
+        Retrieves data from the 75F API and returns the data in a Pandas DataFrame.
+        :param query_string: A Haystack Query to select the desired data.
+        :return: A Pandas Dataframe with the data returned from the API.
+        """
+        reader = ReadByFilter.ReadByFilter(self.username, self.password, self.subscriptionKey, query_string)
+        result = reader.post()
+        df = pd.DataFrame(result["rows"])  # Import into a data frame
+        return df
