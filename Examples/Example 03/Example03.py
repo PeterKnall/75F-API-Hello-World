cec168e28f79c3baef3c67bf53c4c62399b8bd4d
Peter Knall <pknall@aesbuildingservices.com>
2025-11-16 13:06:37 -0500

    Added Example 03.

--- Examples/Example 03/Example03.py
+++ Examples/Example 03/Example03.py
@@ -1,6 +1,6 @@
-import SeventyFiveF.ReadByFilter as Read
+import Tools.SeventyFiveF_API as API
+import Tools.TextTools as TextTools
 import os
-import json
 import pandas as pd
 
 """
@@ -14,7 +14,18 @@ Output files:
 
 site_ref_df.csv             Table of site data returned while searching for all the accessable sites
 
+Note:  Moved get_df() to Tools.SeventyFiveF_API.py
 """
+
+
+# Calculate the zone temperature deviation
+def calculate_temperature_deviation(current_temp, desired_heating_temp, desired_cooling_temp):
+    if current_temp < desired_heating_temp < 999:
+        return current_temp - desired_heating_temp
+    elif desired_cooling_temp < current_temp < 999:
+        return current_temp - desired_cooling_temp
+    else:
+        return 0
 
 # These values are stored in the Windows Environmental Variables so they can be accessed during runtime
 # rather than having them publicly viewable on GitHub.
@@ -24,130 +35,124 @@ subscriptionKey = os.environ.get("75F API Subscription Key")                    
 
 # Regular Expression (Regex) pattern to recognize floating point values surrounded by text.
 # This code assumes there is only one numeric value in the text.
-float_pattern = r'[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?'
+float_pattern = r'[+-]?(\d+(\.\d*)?|\.\d+)'
 
 pd.set_option("display.max_columns", None)                                      # Show all the columns
 pd.set_option("display.max_rows", None)                                         # Show all the rows
 pd.options.mode.chained_assignment = None                                       # Silence a warning from Pandas
 
-def get_df(query_string):
-    """
-    Retrieves data from the 75F API and returns the data in a Pandas DataFrame.
-    :param query_string: A Haystack Query to select the desired data.
-    :return: A Pandas Dataframe with the data returned from the API.
-    """
-    reader = Read.ReadByFilter(username, password, subscriptionKey, query_string)
-    result = reader.post()
-    df = pd.DataFrame(result["rows"])                                           # Import into a data frame
-    return df
+api_tools = API.SeventyFiveF_API(username, password, subscriptionKey)
+text_tools = TextTools.TextTools()
 
-siteId_df = get_df("building and equip")                                        # Returns all buildings w/ siteId
+site_ref_df = api_tools.get_all_site_ids_df()                                   # Returns all buildings w/ siteId
+site_ref_df.to_csv("site_ref_df.csv", header=True, index=False)                 # Save DataFrame to CSV for viewing
 
-site_ref_df = get_df("building and equip")                                      # Returns all buildings w/ siteId
-site_ref_df.to_csv("site_ref_df.csv", header=True, index=False)       # Save DataFrame to CSV for viewing
-
-# Pull the list of Site Ids and Display Names fom the DataFrame.  These lists will be in the same order as they appear
-# in the DataFrame, so items with the same index will correspond to the same entry.
-site_ref_List = site_ref_df["siteRef"].tolist()
-dis_List = site_ref_df["dis"].tolist()
+site_ref_List = site_ref_df["siteRef"].tolist()                                 # copy siteRef's to List
+dis_List = site_ref_df["dis"].tolist()                                          # copy display names to List
 
 is_first = True
 number_of_sites_processed = 0
 
-# Create an empty DataFame outside the scope of the for loop so the DataFrame persists after the loop is complete.
-final_temp_report_df = pd.DataFrame()
+final_temp_report_df = pd.DataFrame()                                           # create report df outside of main loop
 
 for site_ref,name in zip(site_ref_List, dis_List):
-    name_string = name.split("-")[0]
-    siteId = site_ref.split(":")[1]
+    name_string = text_tools.remove_building_equip_text(name)
+    siteId = text_tools.remove_type_infromation_text(site_ref)
 
     # Retrieves the current temperature, desired heating setpoint, and desired cooling setpoint
     query_string = f"temp and not cm and not ti and ((desired and (heating or cooling)) or space) and siteRef==@{siteId}"
     print(f"{number_of_sites_processed} {name_string}")
 
+    #
     equip_df = pd.DataFrame()
     try:
-        equip_df = get_df(query_string)
+        equip_df = api_tools.get_df_by_filter(query_string)
     except Exception as e:
         print(f"Exception while retrieving DataFrame from API: {e}")
+        continue
 
-    df_filtered = pd.DataFrame()
+    # Guard Clause: An exception is generated if the dataframe does not contain the expected columns.
+    # Check for this situation so an exception handler is not necessary.
+    if not  {'equipRef', 'domainName'}.issubset(equip_df.columns):
+        print(f"{name_string} Missing required columns - SKIP!.")
+        equip_df.to_csv(f"_Error {name_string}.csv", header=True, index=False)
+        continue
+
+    df_filtered = pd.DataFrame()    # the DataFrame must exist before adding a column to it
+
+    # Add the equipment id
     df_filtered["equipRef"] = equip_df[equip_df["domainName"] == "currentTemp"]["equipRef"]
-    df_filtered["domainName"] = equip_df[equip_df["equipRef"] == df_filtered["equipRef"]]["domainName"]
-    print(f"df_filtered: {df_filtered}")
+    df_filtered["equip_id"] = [text_tools.remove_type_infromation_text(x) for x in df_filtered["equipRef"]]             # List Comprehension
 
-    break
-    oldText = """
-    ccus["value_string"] = ccus["curVal"].str.extract(float_pattern)[0]
-    ccus["value"] = pd.to_numeric(ccus["value_string"])
+    # Add the display name
+    for i, row in df_filtered.iterrows():
+        # "None Found" occurs for zones that do not have this domainName (desiredTempHeating was a bad choice).
+        df = equip_df[equip_df["domainName"] == "currentTemp"]
+        match = df[df["equipRef"] == row["equipRef"]]
+        if not match.empty:
+            df_filtered.loc[i, "display_name"] = match.iloc[0]["dis"].rsplit("-", 1)[0]
+        else:
+            # Add something to prevent "None" values that crash type conversions
+            df_filtered.loc[i, "display_name"] = "None Found"
 
-    ccus["site_name"] = name_string
-    ccus["counter"] = counter
+    # Add all the "curVal" cells for the rows with the domain name "desiredTempHeating"
+    for i, row in df_filtered.iterrows():
+        df = equip_df[equip_df["domainName"] == "desiredTempHeating"]
+        match = df[df["equipRef"] == row["equipRef"]]
+        if not match.empty:
+            df_filtered.loc[i, "desiredTempHeating"] = match.iloc[0]["curVal"]
+        else:
+            # Add something to prevent "None" values that crash type conversions
+            df_filtered.loc[i, "desiredTempHeating"] = "999"
+    df_filtered["desired_temp_heating"] = pd.to_numeric(df_filtered["desiredTempHeating"].str.extract(float_pattern)[0])
 
-    # PK Pretty Print
-    select_columns = ["counter", "site_name", "dis", "value", "unit"]
-    show_columns = ccus[select_columns]
-    # print(show_columns)
-    if first:
-        first = False
-        show_columns.to_csv("DataFrameToolsTest.csv", header=True, index=False)
+    # Add all the "curVal" cells for the rows with the domain name "currentTemp"
+    for i, row in df_filtered.iterrows():
+        df = equip_df[equip_df["domainName"] == "currentTemp"]
+        match = df[df["equipRef"] == row["equipRef"]]
+        if not match.empty:
+            df_filtered.loc[i, 'currentTemp'] = match.iloc[0]['curVal']
+        else:
+            # Add something to prevent "None" values that crash type conversions
+            df_filtered.loc[i, "currentTemp"] = "999"
+    df_filtered["current_temp"] = pd.to_numeric(df_filtered["currentTemp"].str.extract(float_pattern)[0])
+
+    # Add all the "curVal" cells for the rows with the domain name "desiredTempCooling"
+    for i, row in df_filtered.iterrows():
+        df = equip_df[equip_df["domainName"] == "desiredTempCooling"]
+        match = df[df["equipRef"] == row["equipRef"]]
+        if not match.empty:
+            df_filtered.loc[i, 'desiredTempCooling'] = match.iloc[0]['curVal']
+        else:
+            # Add something to prevent "None" values that crash type conversions
+            df_filtered.loc[i, "desiredTempCooling"] = "999"
+    df_filtered["desired_temp_cooling"] = pd.to_numeric(df_filtered["desiredTempCooling"].str.extract(float_pattern)[0])
+
+    # Calculate the zone temperature deviation
+    df_filtered["temp_deviation"] = [calculate_temperature_deviation(x, y, z)
+        for x, y, z in zip(df_filtered["current_temp"], df_filtered["desired_temp_heating"], df_filtered["desired_temp_cooling"])
+    ]
+
+    # Drop columns not used in report
+    df_filtered.drop('equipRef', axis=1, inplace=True)
+    df_filtered.drop('desiredTempHeating', axis=1, inplace=True)
+    df_filtered.drop('currentTemp', axis=1, inplace=True)
+    df_filtered.drop('desiredTempCooling', axis=1, inplace=True)
+
+    # Append new value to report
+    final_temp_report_df = pd.concat([final_temp_report_df, df_filtered], ignore_index=True)
+
+    # When printing output to file, overwrite the file during the first iteration and append afterwards
+    if is_first:
+        is_first = False
+        df_filtered.to_csv("df_filtered.csv", header=True, index=False)
+        equip_df.to_csv("equip_df.csv", header=True, index=False)
     else:
-        show_columns.to_csv("DataFrameToolsTest.csv", mode='a', header=False, index=False)
-    counter += 1
-    """
+        df_filtered.to_csv("df_filtered.csv", header=True, index=False, mode='a')
+        equip_df.to_csv("equip_df.csv", header=True, index=False, mode='a')
+
+    number_of_sites_processed += 1
 
 
-
-
-
-
-
-
-oldStuff = """
-
-
-# Need to reincorporate the data wrangling items
-
-
-these_rows = results["rows"][0]["data"]                     # Navigate thorough the dict and find the Trend Data List
-df = pd.DataFrame(these_rows)                               # Store that list in a Pandas Data Frame
-
-# Wrangle the date stamp
-df['date_value1'] = df['ts'].str.split(":", n=1).str[1]     # Remove the "n:" portion of the date stamp
-df['date_value2'] = df['date_value1'].str.split(" ", n=1).str[0]  # Remove the city portion of the date stamp (Detroit)
-df['time'] = pd.to_datetime(df['date_value2'], format="ISO8601")  # Convert to datetime object
-
-# Remove unneeded columns / clean-up df
-df = df.drop("val", axis=1)
-df = df.drop("str_value", axis = 1)
-df = df.drop("ts", axis=1)
-df = df.drop("date_value1", axis=1)
-df = df.drop("date_value2", axis=1)
-
-# print(df)
-
-df.plot(x='time', y='value')
-plt.show()
-"""
-
-
-# There are three groups of dicts returned in the JSON object
-# print("-"*20)
-# print(json.dumps(result["metadata"], indent=4))                                 # Pretty print the JSON object
-# print("-"*20)
-# print(json.dumps(result["cols"], indent=4))                                     # Pretty print the JSON object
-# print("-"*20)
-# print(json.dumps(result["rows"], indent=4))                                     # Pretty print the JSON object
-# print("-"*20)
-
-# What is returned in the "rows" section is data about the currentTemp points that satisfy the query above.
-# Next, the interesting data needs to be pulled out and wrangled before being displayed in a graph.
-# For this example, interesting data includes:
-#    * id           "id": "r:9ce9946d-14e2-42ac-aa26-e68af9ac2a0d"
-#    * curVal       "curVal": "n:63.2"
-#    * dis          "dis": "Reservoir Park-VAV-1100-Current Temp"
-#    * ccuRef       "ccuRef": "r:b0f9b93b-3045-4cc5-a899-f474054a502e"
-#    * equipRef     "equipRef": "r:0845c5b9-8e78-4b14-960f-1a8212ec766e"
-#    * floorRef     "floorRef": "r:ebcbfd07-f5a8-4029-ad2f-9d2336ee531a"
-#    * roomRef      "roomRef": "r:dea5e0de-0787-4006-aa8c-51358afb99a5"
-# The "Ref" values will allow the points to be group dynamically later on.  The other rows can deleted.
+print(final_temp_report_df)
+final_temp_report_df.to_csv("final_temp_report_df.csv", header=True, index=False)
