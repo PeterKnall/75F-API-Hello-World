3f9143eb443c8b1962d2101a015cb85d6141be23
Peter Knall <pknall@aesbuildingservices.com>
2025-11-22 16:29:33 -0500

    Initial commit.

---
+++ Test/Tools/test_TextTools.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+
+class TestTextTools(TestCase):
+    def test_remove_building_equip_text(self):
+        self.fail()
+
+    def test_remove_type_information_text(self):
+        self.fail()
+
+    def test_get_float_from_string(self):
+        self.fail()
+
+    def test_get_query_string(self):
+        self.fail()
