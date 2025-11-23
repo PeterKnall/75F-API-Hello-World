af2cf76373eec18b907cb46ceedd66c40fd630e6
Peter Knall <pknall@aesbuildingservices.com>
2025-11-23 11:26:23 -0500

    Update for test results.

--- Tools/TextTools.py
+++ Tools/TextTools.py
@@ -5,32 +5,41 @@ class TextTools:
     def __init__(self):
         pass
 
-    def remove_building_equip_text(self, display_name_string):
+    @staticmethod
+    def remove_building_equip_text(display_name_string):
         """
         Splits the string on "-" and returns the first match.  Effective in removing the "-buildingEquip" text
-        from a string.
+        from a string.  If a "-" is not present, it will return the original string.
+        Example: "Canal Place-buildingEquip" → "Canal Place"
         :param display_name_string: Display Name ('dis')
         :return: Display Name with the "-buildingEquip" removed
         """
+        if not display_name_string:
+            raise Exception("Argument cannot be None.")
         if "-" in display_name_string:
             return display_name_string.split("-")[0]
         else:
             return display_name_string
 
-    def remove_type_information_text(self, type_text):
+    @staticmethod
+    def remove_type_information_text(type_text):
         """
         Removes the type text from an entry.  This text usually appears as "n:", or something similar.
-        For exampe: "r:2f279255-a859-4a2d-974e-1dfcb5f0c627" where the "r:" needs to be removed.
+        For example: "r:2f279255-a859-4a2d-974e-1dfcb5f0c627" where the "r:" needs to be removed.
         :param type_text: Information returned from 75F API
         :return: Information without the type text.
         """
-        if ":" in type_text:
-            return type_text.split(":")[1]
-        else:
-            return type_text
+        if not type_text:
+            raise Exception("Argument cannot be None.")
+        if type_text[1] != ":":
+            raise Exception("Argument has the wrong format.")
+        if len(type_text) != 38:
+            raise Exception("Argument should be 38 characters.")
+        return type_text[2:]
 
 
-    def get_float_from_string(self, float_text):
+    @staticmethod
+    def get_float_from_string(float_text):
         """
         Retrieves the floating point number portion of a string.  Only one floating point number is returned.
         :param float_text: Text with floating point number in it
@@ -38,6 +47,8 @@ class TextTools:
         """
         # Regular Expression (Regex) pattern to recognize floating point values surrounded by text.
         # This code assumes there is only one numeric value in the text.
+        if not float_text:
+            raise Exception("Argument cannot be None.")
         float_pattern = r'[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?'
         match = re.search(float_pattern, float_text)
         if match:
@@ -46,8 +57,9 @@ class TextTools:
             # raise Exception(f"Cannot convert to float: {float_text}")
             return float(-998.0)
 
-    def get_query_string(self, reference_target, reference_string, item_target, item_tag_list):
-        '''
+    @staticmethod
+    def get_query_string(reference_target, reference_string, item_target, item_tag_list):
+        """
         A generic two-part query string builder.  Constructs a query string that looks for the reference target
         at the reference string AND any of the item_targets in the item_tag_list
         :param reference_target: The major query filter (e.g., siteRef or roomRef)
@@ -55,7 +67,9 @@ class TextTools:
         :param item_target: The minor query filter (e.g., domainName)
         :param item_tag_list: The minor filter query list (e.g., [currentTemp, desiredTempCooling, desiredTempHeating]
         :return: The query string to send to the 75F API
-        '''
+        """
+        if not reference_target or not reference_string or not item_target or not item_tag_list:
+            raise Exception("Arguments cannot be None.")
         is_first = True
         query_string = f"{reference_target}==@{reference_string} and ("
         for item in item_tag_list:
