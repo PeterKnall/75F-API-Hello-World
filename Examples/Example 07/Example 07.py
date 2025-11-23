ff3c6e6b14ad1f7e13f583cdfcb74581f560e63e
Peter Knall <pknall@aesbuildingservices.com>
2025-11-23 11:26:39 -0500

    Abbreviated working version.

--- Examples/Example 07/Example 07.py
+++ Examples/Example 07/Example 07.py
@@ -1,10 +1,11 @@
 import os
 import pandas as pd
-import Tools.SeventyFiveF_API as api_tools
+import Tools.SeventyFiveF_API as ApiTools
 import Tools.TextTools as TextTools
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import SeventyFiveF.hisReadMany as rm
+import Tools.DataFrameTools as DataFrameTools
 
 """
 Example 07
@@ -12,64 +13,134 @@ Read in the current temperature, desired heating temperature, and desired coolin
 from several zones and plot them.
 """
 
+# API Access Information
 username = os.environ.get("75F API Username")
 password = os.environ.get("75F API Password")
 subscriptionKey = os.environ.get("75F API Subscription Key")
 
-float_pattern = r'[+-]?(\d+(\.\d*)?|\.\d+)'
+# Storage Location Information
+parent_directory_name = "Pictures"
+os.makedirs(parent_directory_name, exist_ok=True)
+directory_date_format_pattern = "%Y-%m-%d"
 
-tools = api_tools.SeventyFiveF_API(username, password, subscriptionKey)
-text_tools = TextTools.TextTools()
-
-# x-axis display option for matplotlib.pyplot to show the date information in hours and minutes
-plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
-
-# Pull all sites
-# Pull all rooms from site
-
-# Reservoir Park Second Floor VAV 16
-roomRef = "5fd12b52-b7b6-43b7-b958-1d74d9912c92"
+# What am I doing?
 target_trend_domain_names = ['currentTemp', 'desiredTempCooling', 'desiredTempHeating']
+date_range = "yesterday"
 
-# Build hisReadMany query string based on the domain names selected above
-is_first = True
-query_string = f"roomRef==@{roomRef} and ("
-for domain_name in target_trend_domain_names:
-    if is_first:
-        is_first = False
-    else:
-        query_string = query_string + " or "
-    query_string = query_string + f"domainName==@{domain_name}"
-query_string = query_string + ")"
+# Instantiate tools classes
+api_tools = ApiTools.SeventyFiveF_API(username, password, subscriptionKey)
+text_tools = TextTools.TextTools()
+df_tools = DataFrameTools.DataFrameTools()
 
-ids = tools.get_ids_by_query(query_string)                                          # Retrieve a data frame containing the necessary IDs
-
-# Get the historical data with hisReadMany
-date_range = "today"
-results = ""
 try:
-    reader = rm.hisReadMany(username, password, subscriptionKey, ids, date_range)
-    results = reader.post()
+    # Get all Site Ids
+    site_ref_df = api_tools.get_all_site_ids_df()
+    site_ref_df.to_csv("site_ref_df.csv", header=True, index=False)
+    site_ref_list = site_ref_df["siteRef"].tolist()
+    dis_list = site_ref_df["dis"].tolist()
+
+    for site_id, display_name in zip(site_ref_list, dis_list):
+        name_string = text_tools.remove_building_equip_text(display_name)
+        site_string = text_tools.remove_type_information_text(site_id)
+        print(f"{display_name} {site_string} {'x'*10}")
+        site_directory_name = f"{parent_directory_name}\\{name_string}"
+        os.makedirs(site_directory_name, exist_ok=True)
+
+        room_ref_df = api_tools.get_all_rooms_from_site_df(site_string)
+        room_ref_df.to_csv("room_ref_df.csv", header=True, index=False)  # "id" is the roomRef
+        room_ref_list = room_ref_df["id"].tolist()
+
+        if not room_ref_list:
+            print(f"Room list for {display_name} is empty - SKIP")
+            continue
+
+        for room_id in room_ref_list:
+
+            # Generate Query String
+            room_ref = text_tools.remove_type_information_text(room_id)
+            query_string = text_tools.get_query_string("roomRef", room_ref, "domainName", target_trend_domain_names)
+
+            # Retrieve a data frame containing the necessary IDs.  Catch any exceptions and skip entry.
+            point_ref_df = pd.DataFrame()
+            try:
+                point_ref_df = api_tools.get_df_by_filter(query_string)
+            except Exception as e:
+                print(f"Exception while retrieving DataFrame from API: {e}\nQuery string:\n{query_string}")
+                continue
+            point_ref_df.to_csv("point_ref_df.csv", header=True, index=False)
+
+            # Guard Clause: An exception is generated if the dataframe does not contain the expected columns.
+            # Check for this situation so an exception handler is not necessary.
+            if not {'id'}.issubset(point_ref_df.columns):
+                print(f"{name_string} Missing required \'id\' column - SKIP!.")
+                continue
+
+            # Convert ids to a comma separated string to send to rm.hisReadMany
+            point_ref_df["id_ref"] = [text_tools.remove_type_information_text(x) for x in point_ref_df["id"]]
+            point_ref_df["id_ref"] = point_ref_df["id_ref"].astype(str)
+            id_list = point_ref_df["id_ref"].tolist()
+
+            # Get the historical data with hisReadMany
+            results = ""
+            try:
+                reader = rm.hisReadMany(username, password, subscriptionKey, id_list, date_range)
+                results = reader.post()
+            except Exception as e:
+                print(e)
+            historical_df = pd.DataFrame(results["rows"])
+            historical_df.to_csv("historical_df.csv", header=True, index=False)
+
+            # For each trend returned, add it to a plot
+            # fig, ax = plt.subplots()
+            date_directory = ""
+            for row in historical_df.iterrows():
+                trend_df = pd.DataFrame(row[1]["data"])
+                trend_df.to_csv("trend_df.csv", header=True, index=False)
+
+                # Guard Clause: An exception is generated if the dataframe does not contain the expected columns.
+                # Check for this situation so an exception handler is not necessary.
+                if not {'ts','val'}.issubset(trend_df.columns):
+                    print(f"{name_string} Missing required columns \'ts\' or \'val\' - SKIP!.")
+                    continue
+
+                # Wrangle the date stamp
+                try:
+                    trend_df = df_tools.convert_ts_to_datetime_column(trend_df, "ts", "time")
+                except Exception as e:
+                    print(f"Exception while converting DateTime - SKIP: {e}")
+                    continue
+
+                trend_df = df_tools.convert_val_to_numeric_value(trend_df, "val", "value")
+
+                trend_date = trend_df["time"].tolist()[1]
+                trend_date_string = f"{trend_date.year}-{trend_date.month}-{trend_date.day}"
+
+                # Add this Dataframe's information to the current plot, but do not show the plot yet
+                plt.plot(trend_df['time'], trend_df['value'], label=row[1]["domainName"])
+                date_directory = f"{site_directory_name}\\{trend_date_string}"
+
+            title_string = point_ref_df["dis"].tolist()[0].split("-")[2]
+            plt.title(title_string)                             # Add a title
+            plt.ylim(top=85)
+            plt.ylim(bottom=55)
+            plt.xticks(fontsize=8, fontfamily='serif', rotation=45, fontweight='bold', color='black')
+            plt.yticks(fontsize=8, fontfamily='serif', rotation=0, fontweight='bold', color='black')
+            plt.grid(True)
+
+            # x-axis display option for matplotlib.pyplot to show the date information in hours and minutes
+            plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+
+            #plt_filename = f"{date_directory}\\{name_string}_{title_string}.jpeg"
+            #try:
+            #    os.makedirs(date_directory, exist_ok=True)
+            #    plt.savefig(plt_filename)       # Safe to file
+            #except Exception as e:
+            #    print(f"Exception while trying to save data to file - {plt_filename}: {e}")
+            plt.show()
+            plt.clf()
+
+            print(name_string, title_string)
+            break  # Only show one plot for this site
+        break # Only one site
 except Exception as e:
     print(e)
-historical_df = pd.DataFrame(results["rows"])
-historical_df.to_csv("historical_df.csv", header=True, index=False)
-
-# For each trend returned, add it to a plot
-for row in historical_df.iterrows():
-    trend_df = pd.DataFrame(row[1]["data"])
-    trend_df.to_csv("trend_df.csv", header=True, index=False)
-
-    # Wrangle the date stamp
-    trend_df['date_value1'] = trend_df['ts'].str.split(":", n=1).str[1]             # Remove the "n:" portion of the date stamp
-    trend_df['date_value2'] = trend_df['date_value1'].str.split(" ", n=1).str[0]    # Remove the city portion of the date stamp (Detroit)
-    trend_df['time'] = pd.to_datetime(trend_df['date_value2'], format="ISO8601")    # Convert to datetime object
-    # trend_df['time'] = pd.to_datetime(trend_df['ts'].str.split(r"[: ]").str[1], format="ISO8601")    # Convert to datetime object
-
-    # Convert curVal to a float type
-    trend_df["value"] = pd.to_numeric(trend_df["val"].str.extract(float_pattern)[0])
-
-    # Add this Dataframe's information to the current plot, but do not show the plot yet
-    plt.plot(trend_df['time'], trend_df['value'], label=row[1]["domainName"])
-
-plt.show()                                                                          # Show the plot
